fb53f19fb0847798dc6cb7c13fde7363edfed440
aangeles02 <54568738+aangeles02@users.noreply.github.com>
2020-02-26 01:27:19 -0600

    fixed js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 function init(){
-	var but = document.getElementById("button");
 	
+	var FirstName = document.forms["info"]["FirstName"].value;
+	var LastName = document.forms["info"]["LastName"].value;
+	var Email = document.forms["info"]["email"].value;
+	
+		if(FirstName =="")
+		{
+			alert("Please enter your first name");
+		}
+		else if(LastName =="")
+		{
+			alert("Please enter your last name");
+		}
+		else if (Email =="")
+		{
+			alert("Please enter your email address");
+		}
+		else
+		{
+			alert("Thank you for signing up");
+		}
+	}
 
-}
